95d8bca522b9256438b886f4ab9cbd364478e554
c0ncentus <jeremymartin33610@gmail.com>
2024-02-05 10:39:17 +0100

    -up

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-let initialState = {
-  tileSize: 100,
-  selectedTile: 'TILE0000',
-  viewpoints: {},
-  scenes: {}
+
+type Tile = {
+  base: string,
+  layers?: string[],
+  type?: "Ground",
+  data?: any,
+}
+type Map = {
+  table: Tile[][], seq: number | number[], metadata?: any,
+  opt: { zoom: number, size: number, row: number, cols: number }
+}
+type MoovableObject = {};
+
+
+const t: Tile = { base: "brown" };
+
+const initialState: Map = {
+  table: [
+    [t, t, t, t, t],
+    [t, t, t, t, t],
+    [t, t, t, t, t],
+    [t, t, t, t, t],
+    [t, t, t, t, t]
+  ],
+  seq: 1,
 };
 
+export function WorldMapCell({ items }: { items: Map }): any {
+  return <div>
+    <table width={900}>
+      <tbody>
+        {items.table.map((tab) => {
+          return <tr>
+            {tab.map((col) => {
+              <td> </td>
+            })}
+          </tr>
+        })}
+
+      </tbody>
+    </table>
+
+
+  </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(<React.StrictMode></React.StrictMode>);
